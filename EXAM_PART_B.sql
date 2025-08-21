b5d50c0622da4d6f490f15678d1ca84c58a6450b
AriessGit <ariecold@gmail.com>
2025-08-21 20:28:38 +0300

    Update EXAM_PART_B.sql

--- EXAM_PART_B.sql
+++ EXAM_PART_B.sql
@@ -1,14 +1,14 @@
-CREATE TABLE brand (
+CREATE TABLE IF NOT EXISTS brand (
     brand_id SERIAL PRIMARY KEY,
     brand_name VARCHAR(20) UNIQUE NOT NULL
 );
 
-CREATE TABLE resolution (
+CREATE TABLE IF NOT EXISTS resolution (
     resolution_id SERIAL PRIMARY KEY,
     resolution_name VARCHAR(20) UNIQUE NOT NULL
 );
 
-CREATE TABLE panel_type (
+CREATE TABLE IF NOT EXISTS panel_type (
     panel_id SERIAL PRIMARY KEY,
     panel_name VARCHAR(20) UNIQUE NOT NULL
 );
@@ -20,7 +20,8 @@ CREATE TABLE IF NOT EXISTS television (
     brand_id INT,
     FOREIGN KEY (brand_id) REFERENCES brand(brand_id),
     model VARCHAR(50) NOT NULL,
-    screen_size INT NOT NULL CHECK (screen_size > 0),
+    screen_size INT NOT NULL CHECK (screen_size > 18),
+    -- Screen must be bigger than 18 inches because it's the minimum size that I will sell in my store!
     resolution_id INT,
     FOREIGN KEY (resolution_id) REFERENCES resolution(resolution_id),
     price DECIMAL(7,2) NOT NULL CHECK (price > 0),
@@ -28,7 +29,7 @@ CREATE TABLE IF NOT EXISTS television (
     release_year INT NOT NULL,
     smart_tv VARCHAR(3) NOT NULL,
     os VARCHAR(30) 
-    CHECK((smart_tv = 'yes' AND os IS NOT NULL)
+        CHECK((smart_tv = 'yes' AND os IS NOT NULL)
        	 OR (smart_tv = 'no' AND os IS NULL)),
     panel_id INT,
     FOREIGN KEY (panel_id) REFERENCES panel_type(panel_id)
